2a8d56e81f86d9c50b914658d1f6dc37f93d4ac5
Alexey Shabalin <shaba@altlinux.org>
2018-07-24 17:16:34 +0300

    import pgpool-II-3.7.3.tar.gz

--- pgpool-II/src/include/config.h
+++ pgpool-II/src/include/config.h
@@ -273,7 +273,7 @@
 #define PACKAGE_NAME "pgpool-II"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "pgpool-II 3.7.2"
+#define PACKAGE_STRING "pgpool-II 3.7.3"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "pgpool-II"
@@ -282,7 +282,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.7.2"
+#define PACKAGE_VERSION "3.7.3"
 
 /* Define as the return type of signal handlers (`int' or `void'). */
 #define RETSIGTYPE void
@@ -338,7 +338,7 @@
 /* #undef USE_TABLE_LOCK */
 
 /* Version number of package */
-#define VERSION "3.7.2"
+#define VERSION "3.7.3"
 
 /* Define to 1 if `lex' declares `yytext' as a `char *' by default, not a
    `char[]'. */
