1c52e6b43f4853125cec37835074961fd91ea1a3
antioff <zamilov@gmail.com>
2026-04-24 20:09:44 +0300

    Update init repo script

--- github_update.sh
+++ github_update.sh
@@ -1,4 +1,5 @@
-#git push origin --delete $(git ls-remote --refs origin postgres* | cut -d$'\t' -f2)
+PGPOOL_VER=`grep Version pgpool-II.spec | grep -oE '[0-9.]+'`
+
 git add -A
 git commit -m "Update"
 gear-create-tag -f -n "postgrespro-1c-18-v$PGPOOL_VER" -s pgver=postgrespro-1c-18
--- init_repo.sh
+++ init_repo.sh
@@ -111,7 +111,7 @@ chmod +x update.sh
 
 
 cat > "github_update.sh" << \EOF
-#git push origin --delete $(git ls-remote --refs origin postgres* | cut -d$'\t' -f2)
+PGPOOL_VER=`grep Version pgpool-II.spec | grep -oE '[0-9.]+'`
 git add -A
 git commit -m "Update"
 gear-create-tag -f -n "postgrespro-1c-18-v$PGPOOL_VER" -s pgver=postgrespro-1c-18
